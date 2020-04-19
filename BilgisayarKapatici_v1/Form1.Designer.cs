357ae2ac1f4d517610c80179c038db91e2cb9e8f
Osman Koç <info@osmkoc.com>
2020-04-19 13:49:28 +0300

    First commit

---
+++ BilgisayarKapatici_v1/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace BilgisayarKapatici_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Ksaniye = new System.Windows.Forms.ComboBox();
+            this.Kdakika = new System.Windows.Forms.ComboBox();
+            this.Ksaat = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ayarlanmadiLBL = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.programıAçToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gizleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(32, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şu anki saat :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(8, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kapanacak saat :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(296, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(7, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 66);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(134, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(155, 24);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.Ksaniye);
+            this.groupBox1.Controls.Add(this.Kdakika);
+            this.groupBox1.Controls.Add(this.Ksaat);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 100);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "AYARLAYIN";
+            // 
+            // Ksaniye
+            // 
+            this.Ksaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Ksaniye.FormattingEnabled = true;
+            this.Ksaniye.Location = new System.Drawing.Point(246, 63);
+            this.Ksaniye.Name = "Ksaniye";
+            this.Ksaniye.Size = new System.Drawing.Size(43, 24);
+            this.Ksaniye.TabIndex = 17;
+            this.Ksaniye.Text = "00";
+            // 
+            // Kdakika
+            // 
+            this.Kdakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Kdakika.FormattingEnabled = true;
+            this.Kdakika.Location = new System.Drawing.Point(190, 63);
+            this.Kdakika.Name = "Kdakika";
+            this.Kdakika.Size = new System.Drawing.Size(43, 24);
+            this.Kdakika.TabIndex = 16;
+            this.Kdakika.Text = "00";
+            // 
+            // Ksaat
+            // 
+            this.Ksaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Ksaat.FormattingEnabled = true;
+            this.Ksaat.Location = new System.Drawing.Point(133, 63);
+            this.Ksaat.Name = "Ksaat";
+            this.Ksaat.Size = new System.Drawing.Size(43, 24);
+            this.Ksaat.TabIndex = 15;
+            this.Ksaat.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(233, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(176, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = ":";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox2.Controls.Add(this.ayarlanmadiLBL);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 233);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(296, 114);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "AYARLANDI";
+            // 
+            // ayarlanmadiLBL
+            // 
+            this.ayarlanmadiLBL.AutoSize = true;
+            this.ayarlanmadiLBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ayarlanmadiLBL.Location = new System.Drawing.Point(8, 48);
+            this.ayarlanmadiLBL.Name = "ayarlanmadiLBL";
+            this.ayarlanmadiLBL.Size = new System.Drawing.Size(0, 18);
+            this.ayarlanmadiLBL.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.textBox3.Location = new System.Drawing.Point(134, 60);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ShortcutsEnabled = false;
+            this.textBox3.Size = new System.Drawing.Size(150, 24);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(18, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ayarlanan saat :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.textBox2.Location = new System.Drawing.Point(134, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ShortcutsEnabled = false;
+            this.textBox2.Size = new System.Drawing.Size(150, 24);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(43, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Kalan süre :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(197, 354);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "© 2016 • Osman KOÇ";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Bilgisayar Kapatıcı";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programıAçToolStripMenuItem,
+            this.gizleToolStripMenuItem,
+            this.hakkındaToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 114);
+            // 
+            // programıAçToolStripMenuItem
+            // 
+            this.programıAçToolStripMenuItem.Name = "programıAçToolStripMenuItem";
+            this.programıAçToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.programıAçToolStripMenuItem.Text = "Programı Aç";
+            this.programıAçToolStripMenuItem.Click += new System.EventHandler(this.programıAçToolStripMenuItem_Click);
+            // 
+            // gizleToolStripMenuItem
+            // 
+            this.gizleToolStripMenuItem.Name = "gizleToolStripMenuItem";
+            this.gizleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.gizleToolStripMenuItem.Text = "Gizle";
+            this.gizleToolStripMenuItem.Click += new System.EventHandler(this.gizleToolStripMenuItem_Click);
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.hakkındaToolStripMenuItem.Text = "Hakkında";
+            this.hakkındaToolStripMenuItem.Click += new System.EventHandler(this.hakkındaToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(318, 373);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bilgisayar Kapatıcı v1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label ayarlanmadiLBL;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox Ksaniye;
+        private System.Windows.Forms.ComboBox Kdakika;
+        private System.Windows.Forms.ComboBox Ksaat;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem programıAçToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gizleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+    }
+}
+
---
+++ BilgisayarKapatici_v1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;//bilgisayarikapat fonksiyonu için
+
+namespace BilgisayarKapatici_v1
+{
+    public partial class Form1 : Form
+    {
+        bool baslaMi;//buton yazısı Başlat ise true İptal ise false verir
+        Timer tmr = new Timer();
+        DateTime dt = DateTime.Now;
+
+        private void PCkapat()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "C:\\Windows\\system32\\shutdown.exe";
+            psi.Arguments = "-f -s -t 0";
+            Process.Start(psi);
+            this.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.ImageLocation = "ustLogo.png";
+            timer1.Start();
+            label3.Visible = false;
+            label4.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            ayarlanmadiLBL.Text = "Henüz ayarlanmadı.";
+            baslaMi = true;
+
+            for (int i = 0; i < 24; i++)
+            {
+                if (i < 10)
+                    Ksaat.Items.Add("0" + i.ToString());
+                else
+                    Ksaat.Items.Add(i.ToString());
+            }
+            for (int i = 0; i < 60; i++)
+            {
+                if (i < 10)
+                { Kdakika.Items.Add("0" + i.ToString()); Ksaniye.Items.Add("0" + i.ToString()); }
+                else
+                { Kdakika.Items.Add(i.ToString()); Ksaniye.Items.Add(i.ToString()); }
+            }
+
+
+
+            tmr.Interval = 1000;
+            tmr.Tick += (sender, e) =>
+            {
+                TimeSpan diff = dt.Subtract(DateTime.Now);
+                textBox3.Text = string.Format("{0:00}:{1:00}:{2:00}", diff.Hours, diff.Minutes, diff.Seconds);
+                if (dt < DateTime.Now)
+                    ((Timer)sender).Stop();
+                if (tmr.Enabled == false)//geri sayım biterse
+                    PCkapat();
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//Başlat ve İptal Butonu
+            try
+            {
+                int Ksa = Convert.ToInt16(Ksaat.Text);
+                int Kdk = Convert.ToInt16(Kdakika.Text);
+                int Ksn = Convert.ToInt16(Ksaniye.Text);
+                DialogResult Kapat = new DialogResult();
+
+                int Ksa_kalan = Ksa - DateTime.Now.Hour;
+                int Kdk_kalan = Kdk - DateTime.Now.Minute;
+                int Ksn_kalan = Ksn - DateTime.Now.Second;
+                if (Ksa_kalan < 0 || (Ksa_kalan == 0 && Kdk_kalan < 0))
+                    Ksa_kalan += 24;//şu anki saatten az bir zaman girilirse 24 saat eklenecek
+
+                if (baslaMi)//Başlat
+                {
+                    if (Ksa == DateTime.Now.Hour && Kdk == DateTime.Now.Minute)
+                    {
+                        Kapat = MessageBox.Show("Bilgisayar şimdi kapatılacak, onaylıyor musunuz?", "KAPAT", MessageBoxButtons.YesNo);
+                        if (Kapat == System.Windows.Forms.DialogResult.Yes)
+                            PCkapat();
+                    }
+                    else
+                    {
+                        Ksaat.Enabled = false;
+                        Kdakika.Enabled = false;
+                        Ksaniye.Enabled = false;
+
+                        label3.Visible = true;
+                        label4.Visible = true;
+                        textBox2.Visible = true;
+                        textBox3.Visible = true;
+
+                        ayarlanmadiLBL.Visible = false;
+                        baslaMi = false;
+                        button1.Text = "İptal Et";
+                        button1.BackColor = System.Drawing.Color.FromName("Red");
+
+
+                        textBox2.Text = Ksaat.Text + ":" + Kdakika.Text + ":" + Ksaniye.Text;
+                        dt = DateTime.Now.AddHours(Ksa_kalan).AddMinutes(Kdk_kalan).AddSeconds(Ksn_kalan);
+                        tmr.Start();
+
+                        MessageBox.Show("Süre başladı. Program simge durumuna düşürülecek.\nAçıp işlemi iptal etmek için sağ alttan program simgesine çift tıklayın.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Hide();
+                    }
+                }
+                else if (baslaMi == false)//İptal Et
+                {
+                    tmr.Stop();
+                    Ksaat.Enabled = true;
+                    Kdakika.Enabled = true;
+                    Ksaniye.Enabled = true;
+
+                    label3.Visible = false;
+                    label4.Visible = false;
+                    textBox2.Visible = false;
+                    textBox3.Visible = false;
+
+                    ayarlanmadiLBL.Visible = true;
+                    ayarlanmadiLBL.Text = "Henüz ayarlanmadı.";
+                    baslaMi = true;
+                    button1.Text = "Başlat";
+                    button1.BackColor = System.Drawing.Color.FromName("ControlDarkDark");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bilinmeyen bir hata oluştu! \nProgram yapımcısıyla iletişime geçin.", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //textboxa veri girişini engelleme
+            e.Handled = Char.IsLetterOrDigit(e.KeyChar) || Char.IsSymbol(e.KeyChar) || Char.IsPunctuation(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || Char.IsNumber(e.KeyChar);
+        }
+
+        private void Ksaat_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //textboxa sadece sayı girişi
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //formun boyutuyla oynanmaması için
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void programıAçToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void gizleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void hakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu program Osman KOÇ tarafından yapılmıştır. \nDestek için: info@osmkoc.com", "HAKKINDA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult Cikis = new DialogResult();
+            if (!baslaMi)
+            {
+                Cikis = MessageBox.Show("Bilgisayarı kapatma işlemi iptal edilecek, onaylıyor musunuz?", "KAPAT", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if (Cikis == System.Windows.Forms.DialogResult.Yes)
+                {   tmr.Stop(); this.Close();     }
+            }
+            else
+                this.Close();
+        }
+    }
+}
